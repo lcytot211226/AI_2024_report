d0a694347a199e013bee757d86bbb9d90f122fa1
Lin Chin Yung <lcytot211226@gmail.com>
2024-05-31 20:52:21 +0800

    r

--- chessboard.py
+++ chessboard.py
@@ -228,6 +228,8 @@ class chessboard():
         plt.gca().set_aspect('equal', adjustable='box')
         import os
         save_dir = 'picture'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
         file_path = os.path.join(save_dir, f'frame_{self.round:03d}.png')
         plt.savefig(file_path)
         # plt.show()
